09aa520528d0d4bca3c32bca57fd97c0fd5467eb
dangowans <d.gowans@cityssm.on.ca>
2022-04-11 10:03:46 -0400

    remove unnecessary brackets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const toModernJulianDate = (date) => {
     const year = date.getFullYear();
     const januaryFirst = new Date(year, 0, 1);
     const givenDate = new Date(year, date.getMonth(), date.getDate());
-    const day = Math.round((givenDate.getTime() - januaryFirst.getTime()) / (86400000)) + 1;
+    const day = Math.round((givenDate.getTime() - januaryFirst.getTime()) / 86400000) + 1;
     return (year * 1000) + day;
 };
 export default toModernJulianDate;
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ export const toModernJulianDate = (date: Date): number => {
   const januaryFirst = new Date(year, 0, 1);
   const givenDate = new Date(year, date.getMonth(), date.getDate());
 
-  const day = Math.round((givenDate.getTime() - januaryFirst.getTime()) / (86_400_000)) + 1;
+  const day = Math.round((givenDate.getTime() - januaryFirst.getTime()) / 86_400_000) + 1;
 
   return (year * 1000) + day;
 };
